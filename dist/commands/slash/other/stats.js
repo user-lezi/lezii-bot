2c9c8cd7068f88665c9e50b684c3a83ea4ee170b
user-lezi <leleziar@gmail.com>
2024-10-04 16:49:52 +0530

    better /stats

--- dist/commands/slash/other/stats.js
+++ dist/commands/slash/other/stats.js
@@ -4,13 +4,43 @@ const discord_js_1 = require("discord.js");
 exports.default = {
     builder: new discord_js_1.SlashCommandBuilder()
         .setName("stats")
-        .setDescription("Shows the bot's statistics."),
+        .setDescription("Shows the bot's statistics/info."),
     defer: true,
     execute: async (ctx) => {
+        let roundtrip = performance.now();
+        await ctx.reply("*Calculating Roundtrip Latency...*");
+        roundtrip = performance.now() - roundtrip;
         let ping = ctx.client.ws.ping;
-        let embed = ctx.util.embed();
-        embed.setDescription(ctx.join(`**Ping:** ${ping}ms`, `**Guilds:** ${ctx.client.guilds.cache.size}`, `**Users:** ${ctx.client.users.cache.size}`));
-        ctx.reply({ embeds: [embed] });
+        let embed = ctx.util.embed().setTitle(`Bot Statistics`);
+        let count = {
+            server: ctx.client.guilds.cache.size,
+            members: ctx.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0),
+        };
+        /* Pings */
+        embed.addFields({
+            name: "Latency",
+            value: ctx.join(`Bot Latency: ${(0, discord_js_1.bold)(ping + "ms")}`, `Roundtrip Latency: ${(0, discord_js_1.bold)(roundtrip.toFixed(2) + "ms")}`),
+        });
+        /* Statistics */
+        embed.addFields({
+            name: "Statistics",
+            value: ctx.join(`Server Count: ${(0, discord_js_1.bold)(count.server.toLocaleString())}`, `Members Count: ${(0, discord_js_1.bold)(count.members.toLocaleString())}`),
+        });
+        /* Infmarsion */
+        let devs = ctx.client._.devs;
+        let unfetchedDevs = [];
+        for (const dev of devs) {
+            unfetchedDevs.push(ctx.client.users.fetch(dev));
+        }
+        let fetchedDevs = await Promise.all(unfetchedDevs);
+        embed.addFields({
+            name: "Information",
+            value: ctx.join(`Uptime: ${(0, discord_js_1.bold)(ctx.util.parseMS(ctx.client.uptime))}`, `Developer: ${(0, discord_js_1.bold)(fetchedDevs.map((x) => (0, discord_js_1.hyperlink)(`@${x.username}`, `https://discord.com/users/${x.id}`)).join(" | "))}`),
+        });
+        await ctx.reply({
+            content: "*Calculated*",
+            embeds: [embed],
+        });
     },
 };
 //# sourceMappingURL=stats.js.map
--- src/commands/slash/other/stats.ts
+++ src/commands/slash/other/stats.ts
@@ -1,23 +1,60 @@
-import { SlashCommandBuilder } from "discord.js";
+import { bold, hyperlink, SlashCommandBuilder, User } from "discord.js";
 import { SlashCommand } from "../..";
 
 export default {
   builder: new SlashCommandBuilder()
     .setName("stats")
-    .setDescription("Shows the bot's statistics."),
+    .setDescription("Shows the bot's statistics/info."),
   defer: true,
   execute: async (ctx) => {
+    let roundtrip = performance.now();
+    await ctx.reply("*Calculating Roundtrip Latency...*");
+    roundtrip = performance.now() - roundtrip;
     let ping = ctx.client.ws.ping;
-    let embed = ctx.util.embed();
 
-    embed.setDescription(
-      ctx.join(
-        `**Ping:** ${ping}ms`,
-        `**Guilds:** ${ctx.client.guilds.cache.size}`,
-        `**Users:** ${ctx.client.users.cache.size}`,
+    let embed = ctx.util.embed().setTitle(`Bot Statistics`);
+
+    let count = {
+      server: ctx.client.guilds.cache.size,
+      members: ctx.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0),
+    };
+
+    /* Pings */
+    embed.addFields({
+      name: "Latency",
+      value: ctx.join(
+        `Bot Latency: ${bold(ping + "ms")}`,
+        `Roundtrip Latency: ${bold(roundtrip.toFixed(2) + "ms")}`,
       ),
-    );
+    });
 
-    ctx.reply({ embeds: [embed] });
+    /* Statistics */
+    embed.addFields({
+      name: "Statistics",
+      value: ctx.join(
+        `Server Count: ${bold(count.server.toLocaleString())}`,
+        `Members Count: ${bold(count.members.toLocaleString())}`,
+      ),
+    });
+
+    /* Infmarsion */
+    let devs = ctx.client._.devs;
+    let unfetchedDevs = [] as Promise<User>[];
+    for (const dev of devs) {
+      unfetchedDevs.push(ctx.client.users.fetch(dev));
+    }
+    let fetchedDevs = await Promise.all(unfetchedDevs);
+    embed.addFields({
+      name: "Information",
+      value: ctx.join(
+        `Uptime: ${bold(ctx.util.parseMS(ctx.client.uptime))}`,
+        `Developer: ${bold(fetchedDevs.map((x) => hyperlink(`@${x.username}`, `https://discord.com/users/${x.id}`)).join(" | "))}`,
+      ),
+    });
+
+    await ctx.reply({
+      content: "*Calculated*",
+      embeds: [embed],
+    });
   },
 } as SlashCommand<false>;
